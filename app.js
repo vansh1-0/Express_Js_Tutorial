425141095dae0a83d63bbe4b825020651a694b33
vansh1-0 <vansh161976@gmail.com>
2025-08-21 15:57:29 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static("public"))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(__dirname+'home.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname+'about.html')
+})
+
+app.listen(port, () => {
+  console.log(Example app listening on port ${port})
+})
+
+console.log("Started on http://localhost:3000")
