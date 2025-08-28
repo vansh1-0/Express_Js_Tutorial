7b541c7473edb84cc4b14ea646df9b9f634d7b4c
vansh1-0 <vansh161976@gmail.com>
2025-08-28 10:07:01 +0530

    update notes and add query requests

--- app.js
+++ app.js
@@ -2,20 +2,29 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static("public"))
+app.use(express.static("public"))//middle man to connect app to public folder's style.css
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
 app.get('/home', (req, res) => {
-    res.sendFile(__dirname+'home.html')
+    res.sendFile(__dirname+'/home.html')
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(__dirname+'about.html')
+    res.sendFile(__dirname+'/about.html')
 })
 
+app.get('/product/:id',(req,res)=>{
+    var a=req.params.id
+    res.send("product details is " + a)
+})
+
+app.get('/search',(req,res)=>{
+    var a=req.query.q
+    res.send("product details is " + a)
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
